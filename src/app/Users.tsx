185cfcf037d09af1e5bb408f424d80196af464f1
MR-Addict <2750417853@qq.com>
2023-04-20 16:02:34 +0800

    add search bar

--- src/app/Users.tsx
+++ src/app/Users.tsx
@@ -1,55 +1,80 @@
 "use client";
 
+import { TbUsers } from "react-icons/tb";
 import { useMemo, useState } from "react";
 
 import { UserType } from "@/types/user";
 import { UserCard } from "./components";
 
-type FilterType = "all" | "following" | "followers" | "followingbutnotfollowers" | "followersbutnotfollowing";
+interface FilterType {
+  type: "all" | "following" | "followers" | "followingbutnotfollowers" | "followersbutnotfollowing";
+  keywords: string;
+}
 
-function filterUsers(type: FilterType, users: UserType[]) {
-  if (type === "followers") return users.filter((user) => user.follower);
-  else if (type === "following") return users.filter((user) => user.following);
-  else if (type === "followersbutnotfollowing") return users.filter((user) => !user.following && user.follower);
-  else if (type === "followingbutnotfollowers") return users.filter((user) => user.following && !user.follower);
-  else return users;
+function filterUsers(filter: FilterType, users: UserType[]) {
+  let backupUesrs = [...users];
+  if (filter.type === "followers") backupUesrs = users.filter((user) => user.follower);
+  else if (filter.type === "following") backupUesrs = users.filter((user) => user.following);
+  else if (filter.type === "followersbutnotfollowing")
+    backupUesrs = users.filter((user) => !user.following && user.follower);
+  else if (filter.type === "followingbutnotfollowers")
+    backupUesrs = users.filter((user) => user.following && !user.follower);
+
+  type KeyType = "login" | "name" | "email" | "bio" | "company" | "location";
+  const keys: KeyType[] = ["login", "name", "email", "bio", "company", "location"];
+  backupUesrs = backupUesrs.filter((user) => keys.some((key) => user[key]?.toLowerCase().includes(filter.keywords)));
+
+  return backupUesrs;
 }
 
 export default function Users({ token, users }: { token: string; users: UserType[] }) {
-  const [filter, setFilter] = useState<FilterType>("all");
+  const [filter, setFilter] = useState<FilterType>({ type: "all", keywords: "" });
 
   const githubUsers = useMemo(() => filterUsers(filter, users), [users, filter]);
-  const totalCount = useMemo(() => githubUsers.length, [githubUsers]);
   const followersCount = useMemo(() => githubUsers.filter((user) => user.follower).length, [githubUsers]);
   const followingCount = useMemo(() => githubUsers.filter((user) => user.following).length, [githubUsers]);
 
   return (
-    <main className='pb-5 md:py-5 px-5 md:px-48 w-full flex flex-col gap-5'>
-      <div className='z-10 bg-white py-4 border-b border-b-gray-300 w-full flex flex-col md:flex-row justify-between gap-1.5 md:gap-5 sticky top-0 text-sm'>
-        <div className='flex flex-row gap-3'>
-          <p>
-            Total:<span className='text-blue-600'>{totalCount}</span>
+    <main className='px-5 md:px-48 w-full flex flex-col'>
+      <div className='z-10 bg-white py-5 sticky top-0 text-sm w-full flex flex-col gap-1.5 items-end'>
+        <div className='flex flex-row items-center gap-0.5'>
+          <p className='text-gray-500'>
+            <TbUsers />
           </p>
           <p>
-            Following:<span className='text-blue-600'>{followingCount}</span>{" "}
+            {followersCount} <span className='text-gray-500'>followers</span>
           </p>
+          <p>•</p>
           <p>
-            Followers:<span className='text-blue-600'>{followersCount}</span>{" "}
+            {followingCount} <span className='text-gray-500'>following</span>
           </p>
         </div>
 
-        <select
-          value={filter}
-          // @ts-expect-error
-          onChange={(e) => setFilter(e.target.value)}
-          className='border border-gray-300 py-1 md:py-0.5 px-2 outline-none rounded-md'
-        >
-          <option value='all'>All</option>
-          <option value='following'>Following</option>
-          <option value='followers'>Followers</option>
-          <option value='followingbutnotfollowers'>Following But Not Followers</option>
-          <option value='followersbutnotfollowing'>Followers But Not Following</option>
-        </select>
+        <div className='w-full flex flex-col md:flex-row gap-1 md:gap-3'>
+          <div className='w-full'>
+            <input
+              value={filter.keywords}
+              name='filter keywords'
+              placeholder='Search...'
+              onChange={(e) => setFilter({ ...filter, keywords: e.target.value })}
+              className='w-full py-1 px-2 rounded-md border border-gray-300 outline-none focus:border-blue-600'
+            />
+          </div>
+
+          <select
+            value={filter.type}
+            name='filter type'
+            // @ts-expect-error
+            onChange={(e) => setFilter({ ...filter, type: e.target.value })}
+            className='border border-gray-300 py-1 md:py-0.5 px-2 outline-none rounded-md focus:border-blue-600'
+          >
+            <option value='all'>All</option>
+            <option value='following'>Following</option>
+            <option value='followers'>Followers</option>
+            <option value='followingbutnotfollowers'>Following But Not Followers</option>
+            <option value='followersbutnotfollowing'>Followers But Not Following</option>
+          </select>
+        </div>
       </div>
 
       <table>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Users from "./Users";
 import pageSession from "@/lib/auth";
-import { getUsers } from "@/lib/github";
+import { getUsers, readLocalUsers } from "@/lib/github";
 import { Logout, Login } from "@/components";
 
 export const revalidate = 0;
@@ -9,7 +9,8 @@ export default async function Home() {
   const session = await pageSession();
   if (!session) return <Login />;
 
-  const users = await getUsers(session.accessToken);
+  // const users = await getUsers(session.accessToken);
+  const users = readLocalUsers();
   return (
     <>
       <Users token={session.accessToken} users={users} />
