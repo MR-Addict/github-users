e1aad19cd9fe04eae271d5c3adf4c826198c88cc
MR-Addict <2750417853@qq.com>
2023-04-20 22:39:17 +0800

    remove .vscde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Users from "./Users";
 import pageSession from "@/lib/auth";
-import { getUsers, readLocalUsers } from "@/lib/github";
+import { getUsers } from "@/lib/github";
 import { Logout, Login } from "@/components";
 
 export const revalidate = 0;
@@ -9,8 +9,7 @@ export default async function Home() {
   const session = await pageSession();
   if (!session?.accessToken) return <Login />;
 
-  // const users = await getUsers(session.accessToken);
-  const users = readLocalUsers();
+  const users = await getUsers(session.accessToken);
   return (
     <>
       <Users token={session.accessToken} avatar={session.user?.image || ""} users={users} />
